3c09638491640c3026c07935034537f18fbc5346
Tony Shen <tshen815@gmail.com>
2021-05-06 11:29:22 -0500

    implement navbar component with menu items

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,49 +1,51 @@
-import React, {useState, useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { GiBoxingGloveSurprise } from "react-icons/gi";
-import {FaBars, FaTimes} from "react-icons/fa";
+import { FaBars, FaTimes } from "react-icons/fa";
+import Button from "./Button";
 import "../styles/navBar.css";
 
 
-const Navbar = () => {
+const NavBar = () => {
 	const [click, setClick] = useState(false);
 	const [scrolled, setScrolled] = useState(false);
+	const [button, setButton] = useState(true);
 
 	return (
 		<>
- 			<div className={scrolled ? "navbar active" : "navbar"}>
+			<div className={scrolled ? "navbar active" : "navbar"}>
 				<div className="navbar-container container">
 					<Link to="/" className="navbar-logo">
-						<GiBoxingGloveSurprise className="navbar-icon"/> 
+						<GiBoxingGloveSurprise className="navbar-icon" />
 						El Coach Javi
 					</Link>
 					<div className="menu-icon">
-						{click ? <FaTimes/> : <FaBars/>}
+						{click ? <FaTimes /> : <FaBars />}
 					</div>
 					<ul className={click ? "nav-menu active" : "nav-menu"}>
 						<li className="nav-item">
 							<Link to="/" className="nav-links">
-							Home 
+								Home
 							</Link>
 						</li>
 						<li className="nav-item">
 							<Link to="/" className="nav-links">
-							About 
+								About
 							</Link>
 						</li>
 						<li className="nav-item">
 							<Link to="/" className="nav-links">
-							Services 
+								Services
 							</Link>
 						</li>
 						<li className="nav-item">
 							<Link to="/" className="nav-links">
-							Gym 
+								Gym
 							</Link>
 						</li>
 						<li className="nav-item">
 							<Link to="/" className="nav-links">
-							FAQ 
+								FAQ
 							</Link>
 						</li>
 						<li className="nav-btn">
@@ -64,4 +66,4 @@ const Navbar = () => {
 	)
 }
 
-export default Navbar;
+export default NavBar;
